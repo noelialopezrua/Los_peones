21a076fba213457e44be09e8e17e7bbdd7011570
noelialopezrua <47835465+noelialopezrua@users.noreply.github.com>
2022-05-19 16:08:22 +0200

    Add files via upload

---
+++ VERSION BUENA 2/10.05(Esto_no_furula)/src/Torre.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Pieza.h"
+#include "Tablero.h"
+class Torre :public Pieza {
+public:
+	
+	Torre();
+	Torre(int j, float r, int f, int c);//:Pieza(jugador,rojo,verde,azul,radio,fila,columna){};
+
+	void dibuja();
+	bool mov_posible();
+};
